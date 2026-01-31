f2885f1433ee3fd9f5f8475aba8e46df3abe7f42
SHNUYK <99737441+SHNUYK@users.noreply.github.com>
2026-02-01 02:18:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,12 +8,12 @@ from damage import roll_damage
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command("roll"))
+@dp.message(Command("r"))
 async def roll_handler(message: types.Message):
     result = roll_check(message.text)
     await message.reply(result)
 
-@dp.message(Command("damage"))
+@dp.message(Command("d"))
 async def damage_handler(message: types.Message):
     result = roll_damage(message.text)
     await message.reply(result)
